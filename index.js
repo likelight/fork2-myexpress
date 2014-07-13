7aff052b07a7f3d3564a000b7b2bfd8421da89b8
likelight <likelight3@gmail.com>
2014-07-13 22:53:37 +0800

    finish lesson 9

--- index.js
+++ index.js
@@ -1,14 +1,187 @@
 var http = require("http");
 
 module.exports = function(){
-	var app = function(req,res){
-		res.statusCode = 404;
-		res.end();
-	
+			
+	//add an app to deal with request
+	var app = function(req,res,next){
+		req.app = app;
+		return app.handle(req,res,next);
+	}
+
+
+	app.handle = function(req,res,next){
+		var index = 0;
+		var stack = app.stack;
+		if(stack.length == 0){
+			res.statusCode = 404;
+			res.end("Not found");
+		}else{	
+			
+			var next = function(err){
+			
+				var middleware = stack[index];
+
+				if(typeof(middleware) == 'undefined'){
+					
+					if(err){
+						res.statusCode = 500;
+						res.end();
+					}else{
+						res.statusCode = 404;
+						res.end("not found");
+					}
+					return;
+				}else{
+					
+					var arg_length = middleware.length;
+					index ++;
+					try{
+						if(err){
+						// if quified the format 
+							if(arg_length == 4){
+								middleware(err,req,res,next);
+							}else{
+								next(err);
+							}
+						}else{
+						
+							// if not err
+							if(arg_length < 4){
+								middleware(req,res,next);
+							}else{
+								next();
+							}
+						}
+					}catch(err){
+						res.statusCode = 500;
+						res.end();
+					}
+					
+
+				}
+				
+			};
+			next();
+		}
+
 	};
+
+	app.stack = [];
+		
+	//add listen function to listen port
 	app.listen = function(port,done){
 		var server = http.createServer(app);
 		return server.listen(port,done);
 	};
+	// stack is a middleware collections
+	
+	app.use = function(middleware){
+		var middleware;
+		if(middleware.hasOwnProperty("stack")){
+			for(var i in middleware.stack){
+				this.stack.push(middleware.stack[i]);
+			}
+		}else{
+				this.stack.push(middleware);
+		}
+		
+	};
+
 	return app;
 }
+
+
+// var http = require('http');
+
+// var express = function(){
+
+//     var app = function(request, response){
+
+//         // middleware stack
+//         var stack = app.stack;
+//         // stack progress num
+//         var stackProgress = 0;
+
+//         if(stack.length === 0){
+//             // no middleware
+//             response.statusCode = 404;
+//             response.end('not found');
+//         }else{
+
+//             var next = function(err){
+
+//                 var middleware = stack[stackProgress];
+                
+//                 if(typeof middleware === 'undefined'){
+//                     if(err){
+//                         response.statusCode = 500;
+//                         response.end();
+//                     }else{
+//                         response.statusCode = 404;
+//                         response.end('not found');
+//                     };
+//                     return;
+//                 }else{
+//                     var argvLength = middleware.length;
+//                     stackProgress ++;
+//                     try{
+//                         if(err){
+//                             if(argvLength === 4){
+//                                 middleware(err, request, response, next);
+//                             }else{
+//                                 next(err);
+//                             }
+//                         }else{
+//                             if(argvLength < 4){
+//                                 middleware(request, response, next);
+//                             }else{
+//                                 next();
+//                             }
+//                         }
+//                     }catch(err){
+//                         response.statusCode = 500;
+//                         response.end();
+//                     }
+//                 }
+                
+//             };
+//             // start call middleware
+//             next();
+//         }
+
+//     };
+
+//     app.stack = [];
+
+//     app.listen = function(port, callback){
+
+//         var port = port || 4000;
+//         var callback = callback || function(){};
+
+//         // creat server
+//         var server = http.createServer(this);
+//         server.listen(port, function(){
+//             callback();
+//         });
+
+//         return server;
+
+//     };
+    
+//     app.use = function(middleWare){
+//         // console.log(middleWare instanceof app)
+//         if(middleWare.hasOwnProperty('stack')){
+//             for(var i in middleWare.stack){
+//                 this.stack.push(middleWare.stack[i]);
+//             }
+//         }else{
+//             this.stack.push(middleWare);
+//         }
+
+//     };
+
+//     return app;
+
+// };
+
+// module.exports = express;
--- test/app_spec.js
+++ test/app_spec.js
@@ -31,5 +31,193 @@ describe("app",function(){
 			request("http://localhost:4000").get("/foo").expect(404).end(done);
 		});
 	});
+
+	//test use function 
+	describe(".use",function(){
+		var m1 = function(){};
+		var m2 = function(){};
+
+		it("should be able to add middlewares to stack",function(){
+			app.use(m1);
+			app.use(m2);
+			expect(app.stack.length).to.equal(2);
+
+		});
+	});
+
+	describe("calling middlewares stack",function(){
+		var app1;
+		beforeEach(function(){
+			app1 = new express();
+		});
+				
+		it("should be able to call a single middlewares",function(done){
+			var m1 = function(req,res,next){
+				res.end("hello from m1");
+			};
+			app1.use(m1);
+			request(app1)
+			.get("/")
+			.expect("hello from m1")
+			.end(done);
+		});
+
+		it("should be able to call next to go to the next middlewares",function(done){
+			var m1 = function(req,res,next){
+				//res.send("hello from m1\n");
+				next();
+			};
+			var m2 = function(req,res,next){
+				res.end("hello from m2");
+			} 
+			app1.use(m1);
+			app1.use(m2);
+			request(app1)
+			.get("/")
+			.expect("hello from m2")
+			.end(done)
+		});
+
+		it("should 404 at the end of middlewares chain",function(done){
+			var m1 = function(req,res,next){
+				next();
+			}
+			var m2 = function(req,res,next){
+				next();
+			}
+			app1.use(m1);
+			app1.use(m2);
+			request(app1)
+			.get("/")
+			.expect(404)
+			.end(done);
+		});
+
+		it("should 404 if no middleware is added",function(done){
+			app1.stack = [];
+			request(app1)
+			.get("/")
+			.expect(404)
+			.end(done);
+		});
+
+	});
+
+	describe("Implement Error Handling",function(){
+		var app;
+		beforeEach(function(){
+			app = new express();
+		});
+		it("should return 500 for unhandled error",function(done){
+			var m1 = function(req,res,next){
+				next(new Error("boom!"));
+			};
+			app.use(m1);
+			request(app)
+			.get("/")
+			.expect(500)
+			.end(done);
+		});
+
+		it("should return 500 for uncaught error",function(done){
+			var m1 = function(req,res,next){
+				throw new Error("boom!");
+			};
+			app.use(m1);
+			request(app)
+			.get("/")
+			.expect(500)
+			.end(done);
+		});
+
+		it('should skip error handlers when next is called without an error', function (done) {
+			var m1 = function (req, res, next) {
+				next();
+			}
+
+			var e1 = function (err, req, res, next) {
+				//res.end('m1');
+			}
+
+			var m2 = function (req, res,next) {
+				res.end('m2');
+			};
+
+			app.use(m1);
+			app.use(e1);
+			app.use(m2);
+
+			request(app)
+			.get('/')
+			.expect('m2')
+			.end(done);
+		});
+
+		it('should skip normal middlewares if next is called with an error',function(done){
+			var m1 = function(req,res,next) {
+  				next(new Error("boom!"));
+			}
+
+			var m2 = function(req,res,next) {
+  				res.end("m2");
+			}
+
+			var e1 = function(err,req,res,next) {
+  				res.end("e1");
+			}
+
+			app.use(m1);
+			app.use(m2); // should skip this. will timeout if called.
+			app.use(e1);
+
+			request(app)
+			.get("/")
+			.expect("e1")
+			.end(done);
+		});
+
+	});
+
+	describe("App embedding as middleware",function(){
+		 var app1,app2;
+		 beforeEach(function() {
+    		app1 = new express();
+    		app2 = new express();
+  		});
+		it("should pass unhandled request to parent",function(done){
+		
+			function m2(req,res,next){
+				res.end("m2");
+			}
+			app1.use(app2);
+			app1.use(m2);
+
+			request(app1)
+			.get("/")
+			.expect("m2")
+			.end(done);
+
+		});
+
+		it("should pass unhandled error to parent",function(done){
+
+			function m1(req,res,next) {
+  				next("m1 error");
+			}
+
+			function e1(err,req,res,next) {
+  				res.end(err);
+			}
+			app2.use(m1);
+			app1.use(app2);
+			app1.use(e1);
+			request(app1)
+			.get("/")
+			.expect("m1 error")
+			.end(done);
+		});
+
+	});
+
 });
 
