375cbc0facad7a2a6c70c10dfbbf15cef356d007
likelight <likelight3@gmail.com>
2014-07-26 18:34:29 +0800

    修改路径处理方式，添加修改以匹配subapp下的路径处理

--- index.js
+++ index.js
@@ -1,109 +1,121 @@
 var http = require("http");
 var Layer = require("./lib/layer.js");
 
-module.exports = function(){
-			
-	//add an app to deal with request
-	var app = function(req,res,next){
-		req.app = app;
-		return app.handle(req,res,next);
-	}
+module.exports = function() {
 
-	app.handle = function(req,res,next){
-		var index = 0;
-		var stack = app.stack;
+    //add an app to deal with request
+    var app = function(req, res, next) {
+        req.app = app;
+        return app.handle(req, res, next);
+    }
+    //用于存储app.use记录的中间件
+    app.stack = [];
+    //handle method to deal with the real request
+    app.handle = function(req, res, outnext) {
+        var stack_index = 0; //stack的索引
+        var stack = app.stack;
+        var removed = '';
+        req.params = [];
+        var old_url = req.url; //记录初次访问路径，以供访问路径在经过subapp时候的修改还原
+        var url_modified_status = 0; // url修改标志符 0：未修改；1：已修改
+        //获得访问路径			
+        var next = function(err) {
+            var middleware_layer = stack[stack_index++];
+            //若路径被修改过时，进行还原
+            if (url_modified_status) {
+                url_modified_status = 0;
+                req.url = old_url;
+            }
 
-		if(stack.length == 0){
-			res.statusCode = 404;
-			res.end("Not found");
-		}else{
-			//获得访问路径
-			var url = req.url;
-			var next = function(err){
-				var middleware_layer = stack[index];
-				var match_result = middleware_layer.match(url);
-				req.params = {};
-				if(match_result){
-					// 如果匹配路径则执行对应操作
-					middleware = middleware_layer.handle;
-					//获取详细参数
-					req.params = match_result.params;
+            if (middleware_layer) {
+                var result = middleware_layer.match(req.url);
+                if (result) {
+                    req.params = result.params;
+                    var middleware = middleware_layer.handle;
+                } else {
+                    req.params = {};
+                    return next(err);
+                }
+                //含有subapp的情况
+                if (typeof middleware.handle == 'function') {
+                    var remote = middleware_layer.path;
+                    //含有subapp时需要对路径进行截取，去除前端路径
+                    req.url = req.url.replace(remote, "");
+                    if (req.url[0] != '/') {
+                        req.url = '/';
+                    }
+                    url_modified_status = 1;
+                    middleware.handle(req, res, next);
+                } else {
+                	//正常非subapp情况
+                    try {
+                        //普通middle layer
+                        if (err) {
+                            if (middleware.length == 4) {
+                                middleware(err, req, res, next);
+                            } else {
+                                next(err);
+                            }
+                        } else {
 
-					if(typeof(middleware) == 'undefined'){
-						if(err){
-							res.statusCode = 500;
-							res.end();
-						}else{
-							res.statusCode = 404;
-							res.end("not found");
-						}
-						return;
-					}else{
-						if(typeof middleware.handle === "function"){
-							req.old_url = req.url;
-							var path = middleware_layer.path;
-							req.url = req.url.replace(path,"");
-							console.log("req is "+req.url);
-						}else{
-							req.url = match_result["path"];
-						}
-						var arg_length = middleware.length;
-						index ++;
-						try{
-							if(err){
-							// if quified the format 
-								if(arg_length == 4){
-									middleware(err,req,res,next);
-								}else{
-									next(err);
-								}
-							}else{
-								// if not err
-								if(arg_length == 3){
-									middleware(req,res,next);
-								}else if(arg_length == 2){
-									middleware(req,res);
-								}else{
-									next();
-								}
-							}
-						}catch(err){
-							res.statusCode = 500;
-							res.end();
-						}
-					}
-				}else{
-					//否则 next
-					index++;
-					next();
-				}
+                            if (middleware.length < 4) {
+                                middleware(req, res, next);
+                            } else {
+                                next();
+                            }
+                        }
+                    } catch(err) {                
+                        next(err);
+                    }
 
-		   };
-		   next();
-		}
+                }
 
-	};
+            } else {
+                //没有middlerware时直接返回
+                if (err) {
+                	//若是subapp处理逻辑返回parentapp的处理逻辑
+                    if (outnext) {
+                        outnext(err);
+                    } else {
+                        res.statusCode = 500;
+                        res.end();
+                    }
+                } else {
+                	//若是subapp处理逻辑返回parentapp的处理逻辑
+                    if (outnext) {
+                        outnext();
+                    } else {
+                        res.statusCode = 404;
+                        res.end();
+                    }
+                }
 
-	app.stack = [];
-		
-	//add listen function to listen port
-	app.listen = function(port,done){
-		var server = http.createServer(app);
-		return server.listen(port,done);
-	};
-	// stack is a middleware collections
-	
-	app.use = function(path,middleware){
-		if('string' != typeof(path)){
-			middleware = path;
-			path = '/';
-		}
-		var layer = new Layer(path,middleware);
-		this.stack.push(layer);
-		return app;		
-	};
+                return;
 
-	return app;
+            }
+
+        };
+
+        next();
+    };
+
+
+    //add listen function to listen port
+    app.listen = function(port, done) {
+        var server = http.createServer(app);
+        return server.listen(port, done);
+    };
+    // stack is a middleware collections
+    app.use = function(path, middleware) {
+        //
+        if ('string' !== typeof(path)) {
+            middleware = path;
+            path = '';
+        }
+        var layer = new Layer(path, middleware);
+        this.stack.push(layer);
+        return app;
+    };
+
+    return app;
 }
-
-
